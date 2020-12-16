27a4d41e371503048b9906a4f7dfc7f4b87b7b2e
Elisa Soto <elisasotoperez@gmail.com>
2020-12-16 14:48:53 +0100

    map dinamcally changing on clic

--- src/barcharts.js
+++ src/barcharts.js
@@ -1,77 +1,77 @@
 // function that deals with the country data to create a barchart
 
-function handleCountryData(country_name) {
-    let countryDate = [];
-    let yearCases = [];
-  
-    const countryDailyCases = covidData.filter((country) => {
-      if (country_name === country.name) {
-        return {
-          date: country.date,
-          dailyCases: country.dailyCases,
-        };
-      }
-    });
-    countryDailyCases.sort((a, b) => (a.date > b.date ? 1 : -1));
-  
-    countryDailyCases.forEach((country) => countryDate.push(country.date) && yearCases.push(Number(country.dailyCases)));
-    
-    return [countryDate, yearCases];
-  }
-  
-  // 2.2. Function that creates my barchart
-  function paintBarGraph() {
-    const oneCountry = handleCountryData(country_name);
-    const dayBarChartData = {
-      labels: oneCountry[0],
-      series: [oneCountry[1]],
-    };
-    new Chartist.Bar('#daily-bar-chart', dayBarChartData);
-  }
-  
-  // SECTION 2.- CREATING ADDITIONAL BARCHARTS
-  
-  // 2.1. Formatting data
-  
-  const result = covidData.filter((country) => {
-    const date = '01/12/2020';
-    if (date === country.date) {
+function handleCountryData(countryName) {
+  let countryDate = [];
+  let yearCases = [];
+
+  const countryDailyCases = covidData.filter((country) => {
+    if (countryName === country.name) {
       return {
-        country,
+        date: country.date,
+        dailyCases: country.dailyCases,
       };
     }
   });
-  
-  result.sort((a, b) => b.casesPerMile - a.casesPerMile);
-  
-  let countries = [];
-  const getCountries = result.forEach((country) => {
-    return countries.push(country.name);
-  });
-  
-  let casesPerMile = [];
-  const getCasesPerMile = result.forEach((country) => {
-    return casesPerMile.push(Number(country.casesPerMile));
-  });
-  
-  // 2.2. Creating a Barcahart
-  
-  const barChartData = {
-    labels: countries,
-    series: [casesPerMile],
+  countryDailyCases.sort((a, b) => (a.date > b.date ? 1 : -1));
+
+  countryDailyCases.forEach((country) => countryDate.push(country.date) && yearCases.push(Number(country.dailyCases)));
+
+  return [countryDate, yearCases];
+}
+
+// 2.2. Function that creates my barchart
+function paintBarGraph() {
+  const oneCountry = handleCountryData(countryName);
+  const dayBarChartData = {
+    labels: oneCountry[0],
+    series: [oneCountry[1]],
   };
-  
-  // barchart options
-  
-  const barChartOptions = {
-    axisX: {
-      showGrid: false,
-    },
-    axisY: {
-      showGrid: false,
-      scaleMinSpace: 0,
-    },
-    seriesBarDistance: 0,
-  };
-  
-  new Chartist.Bar('#bar-chart', barChartData, barChartOptions);
+  new Chartist.Bar('#daily-bar-chart', dayBarChartData);
+}
+
+// SECTION 2.- CREATING ADDITIONAL BARCHARTS
+
+// 2.1. Formatting data
+
+const result = covidData.filter((country) => {
+  const date = '01/12/2020';
+  if (date === country.date) {
+    return {
+      country,
+    };
+  }
+});
+
+result.sort((a, b) => b.casesPerMile - a.casesPerMile);
+
+let countries = [];
+const getCountries = result.forEach((country) => {
+  return countries.push(country.name);
+});
+
+let casesPerMile = [];
+const getCasesPerMile = result.forEach((country) => {
+  return casesPerMile.push(Number(country.casesPerMile));
+});
+
+// 2.2. Creating a Barcahart
+
+const barChartData = {
+  labels: countries,
+  series: [casesPerMile],
+};
+
+// barchart options
+
+const barChartOptions = {
+  axisX: {
+    showGrid: false,
+  },
+  axisY: {
+    showGrid: false,
+    scaleMinSpace: 0,
+  },
+  seriesBarDistance: 0,
+};
+
+new Chartist.Bar('#bar-chart', barChartData, barChartOptions);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 
 
-let country_name = '';
+let countryName = '';
 
 // 3. Visualising map and handle the changes in select
 const goToSelectCountry = async () => {
   const e = document.getElementById('countryFilterSelect');
-  country_name = e.options[e.selectedIndex].text;
+  countryName = e.options[e.selectedIndex].text;
   const country_Code = e.options[e.selectedIndex].value;
   const REST_PLACES_URL = `.json?country=${country_Code}&access_token=${MY_ACCESS_TOKEN}`;
-  const userURL = `${MAPBOX_PLACES_API}${country_name}${REST_PLACES_URL}`;
+  const userURL = `${MAPBOX_PLACES_API}${countryName}${REST_PLACES_URL}`;
   const response = await fetch(userURL);
   const data = await response.json();
   const result = data;
@@ -17,7 +17,7 @@ const goToSelectCountry = async () => {
   mymap.flyTo(latlong);
 
   L.marker(latlong)
-    .bindPopup(`<b>${country_name}</b>`)
+    .bindPopup(`<b>${countryName}</b>`)
     .addTo(mymap)
     .on('click', () => paintBarGraph());
 };
